03f18f7bed9373b6d4c4172969a6cf23b7118a04
VoidBaroness <52724070+GamingInfinite@users.noreply.github.com>
2023-12-18 15:11:30 -0600

    Skeleton of QR Code Generator App

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import {invoke} from "@tauri-apps/api/tauri"
+</script>
+
+<div class="flex flex-row justify-center my-2">
+  <div class="flex flex-col">
+    <div class="flex flex-row gap-2">
+      <input
+        type="text"
+        placeholder="Put URL Here"
+        class="input input-bordered w-full max-w-xs"
+      />
+      <button class="btn btn-primary" on:click={()=> {
+        invoke("create_svg")
+      }}>Generate QR Code</button>
+    </div>
+  </div>
+</div>
